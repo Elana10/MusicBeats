1d98b1cd765ab93eee072ed1167d10eefcac6ae0
Elana10 <135548464+Elana10@users.noreply.github.com>
2024-05-29 17:25:20 -0500

    Update GenerateAppleDevToken.js
    require('../config.js');

--- backend-music-beats/routes/GenerateAppleDevToken.js
+++ backend-music-beats/routes/GenerateAppleDevToken.js
@@ -7,7 +7,7 @@ router = new express.Router();
 
 router.get('/', async function (req, res, next) {
 
-const {TEAM_ID, KID, APPLE_PRIVATE_KEY } = require('../secret');
+const {TEAM_ID, KID, APPLE_PRIVATE_KEY } = require('../config.js');
 const expiration = 72000;
 const currentTime = Math.floor(Date.now() /1000);
 const expirationTime = currentTime + expiration;
