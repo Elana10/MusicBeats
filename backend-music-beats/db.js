05653b2b42299ebf8cf6d9e1629ebbfdc6cf0d89
Elana10 <135548464+Elana10@users.noreply.github.com>
2024-05-29 15:45:39 -0500

    Update db.js
    database connection updated

--- backend-music-beats/db.js
+++ backend-music-beats/db.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
     db = new Client({
       // host : '/var/run/postgresql',
       // database: getDatabaseUri(),
-        connectionString : getDatabaseUri(),
+        connectionString : "postgresql:///musicbeats",
       ssl: {
         rejectUnauthorized: false
       }
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
     db = new Client({
       // host : '/var/run/postgresql',
       // database: getDatabaseUri()
-        connectionString : getDatabaseUri(),
+        connectionString : "postgresql:///musicbeats",
     });
   }
   
