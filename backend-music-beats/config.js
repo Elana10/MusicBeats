c7e45aee54e243d1b96475eb01a0301ec5557a45
Elana Felty <elanafelty@gmail.com>
2024-05-29 15:15:42 -0500

    added variable names to the dotenv

--- backend-music-beats/config.js
+++ backend-music-beats/config.js
@@ -14,4 +14,9 @@ const SECRET_JWT_KEY = "FlamingoParty!"
 
 const BCRYPT_WORK_FACTOR = 12;
 
+const CHAT_KEY = process.env.CHAT_KEY
+const APPLE_PRIVATE_KEY = process.env.APPLE_PRIVATE_KEY
+const KID = process.env.KID
+const TEAM_ID = process.env.TEAM_ID
+
 module.exports = {PORT, BASE_URL, SECRET_JWT_KEY, BCRYPT_WORK_FACTOR, getDatabaseUri}
